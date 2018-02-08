590093d7e8633e56f122e3e06a7eeb54f093a94e
newpolaris <newpolaris@gmail.com>
2018-02-08 17:41:25 +0900

    Split profiler class

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,7 @@
 #include <imgui/imgui_impl_glfw_gl3.h>
 
 #include <tools/stb_image.h>
+#include <tools/SimpleProfile.h>
 
 #include <tools/TCamera.hpp>
 #include <tools/Timer.hpp>
@@ -44,64 +45,6 @@
 #include <memory>
 #include <vector>
 #include <algorithm>
-
-#include <chrono>
-
-using std::chrono::high_resolution_clock;
-class SimpleProfileGL
-{
-public:
-    SimpleProfileGL(const std::string& name): m_name(name)
-    {
-		m_queryCPU[0] = high_resolution_clock::now();
-        // generate two queries
-        glGenQueries(2, m_queryID);
-
-        // issue the first query
-        // Records the time only after all previous 
-        // commands have been completed
-        glQueryCounter(m_queryID[0], GL_TIMESTAMP);
-
-    }
-    ~SimpleProfileGL()
-    {
-		using namespace std::chrono;
-		using millisec = duration<float, std::milli>;
-
-		m_queryCPU[1] = high_resolution_clock::now();
-		auto cpuTime = duration_cast<millisec>(m_queryCPU[1] - m_queryCPU[0]);
-
-        // issue the second query
-        // records the time when the sequence of OpenGL 
-        // commands has been fully executed
-        glQueryCounter(m_queryID[1], GL_TIMESTAMP);
-
-        // wait until the results are available
-        GLint stopTimerAvailable = 0;
-        while(!stopTimerAvailable) {
-            glGetQueryObjectiv(m_queryID[1],
-                GL_QUERY_RESULT_AVAILABLE,
-                &stopTimerAvailable);
-        }
-
-        // get query results
-        GLuint64 startTime, stopTime;
-        glGetQueryObjectui64v(m_queryID[0], GL_QUERY_RESULT, &startTime);
-        glGetQueryObjectui64v(m_queryID[1], GL_QUERY_RESULT, &stopTime);
-
-        printf("Time spent on the CPU %s: %f ms\n", m_name.c_str(), cpuTime.count());
-        printf("Time spent on the GPU %s: %f ms\n", m_name.c_str(), (stopTime - startTime) / 1000000.0);
-
-        fflush(stdout);
-        glDeleteQueries(2, m_queryID);
-    }
-
-	high_resolution_clock::time_point m_queryCPU[2];
-    std::string m_name;
-    unsigned int m_queryID[2];
-};
-
-#define PROFILEGL(name) SimpleProfileGL __profile(name)
 
 namespace {
     // lights
